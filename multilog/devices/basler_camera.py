161970022527b0b78a9fe28a49f5e3a855d4562e
Kaspars Dadzis <57000869+kdadzis@users.noreply.github.com>
2025-07-29 12:55:30 +0200

    Revert "Update of #29"

--- multilog/devices/basler_camera.py
+++ multilog/devices/basler_camera.py
@@ -2,8 +2,6 @@ import datetime
 import logging
 import os
 import shutil
-import numpy as np
-import time
 
 logger = logging.getLogger(__name__)
 try:
@@ -38,56 +36,48 @@ class BaslerCamera:
         device_number = config["device-number"]
         self.fileformat = config["file-format"]
         tl_factory = pylon.TlFactory.GetInstance()
-
-        self.enableDataStorage = True
-        self.debugMode         = False
-        self.parameterChange   = False
-
-        try:
-            self.device_name = tl_factory.EnumerateDevices()[
-                device_number
-            ].GetFriendlyName()
-            self._device = pylon.InstantCamera()
-            self._device.Attach(
-                tl_factory.CreateDevice(tl_factory.EnumerateDevices()[device_number])
-            )
-            self._converter = pylon.ImageFormatConverter()
-            self._converter.OutputPixelFormat = pylon.PixelType_RGB8packed # change B and R if colors are wrong
-            self._converter.OutputBitAlignment = pylon.OutputBitAlignment_MsbAligned
-            self._name = tl_factory.EnumerateDevices()[device_number].GetFriendlyName()
-            self._model_number = self._name.split(" ")[1]
-            self._device_class = tl_factory.EnumerateDevices()[
-                device_number
-            ].GetDeviceClass()
-
-            self._device.Open()
-
-            # Set inital exposure_time and make it an externaly usable variable
-            self.exposure_time = config["exposure-time"]
-            self._set_exposure_time(self.exposure_time)
-
-            # Set inital frame_rate and make it an externaly usable variable
-            self.frame_rate = config["frame-rate-camera"]
-            self.set_frame_rate(self.frame_rate)
-            
-            self._device.StartGrabbing(pylon.GrabStrategy_LatestImageOnly)
-            #self._device.StartGrabbing(pylon.GrabStrategy_UpcomingImage)
-        except Exception as e:
-            self.debugMode = True
-
-            logger.error(f"Basler Camera was not mounted: {e}")
-            
-            self.device_name = "DEBUG-Camera"
-            self._name = "DEBUG-Camera"
-            self._model_number = 0
-
-            self.exposure_time = config["exposure-time"]
-            self.frame_rate    = config["frame-rate-camera"]
-
-        self.frame_rate_multilog = config["frame-rate-multilog"]
-        self.timeLastPhoto = datetime.datetime.now(datetime.timezone.utc).astimezone()
+        self.device_name = tl_factory.EnumerateDevices()[
+            device_number
+        ].GetFriendlyName()
+        self._device = pylon.InstantCamera()
+        self._device.Attach(
+            tl_factory.CreateDevice(tl_factory.EnumerateDevices()[device_number])
+        )
+        self._converter = pylon.ImageFormatConverter()
+        self._converter.OutputPixelFormat = pylon.PixelType_RGB8packed # change B and R if colors are wrong
+        self._converter.OutputBitAlignment = pylon.OutputBitAlignment_MsbAligned
+        self._name = tl_factory.EnumerateDevices()[device_number].GetFriendlyName()
+        self._model_number = self._name.split(" ")[1]
+        self._device_class = tl_factory.EnumerateDevices()[
+            device_number
+        ].GetDeviceClass()
+        self._set_exposure_time(config["exposure-time"])
+        self.set_frame_rate(config["frame-rate"])
+        self._device.Open()
+        self._device.StartGrabbing(pylon.GrabStrategy_LatestImageOnly)
+        # self._device.StartGrabbing(pylon.GrabStrategy_UpcomingImage)
         self.meas_data = []
         self.image_counter = 1
+
+    def _set_exposure_time(self, exposure_time):
+        """Set exposure time."""
+        logger.info(f"{self.name} - setting exposure time {exposure_time}")
+        self._device.Open()
+        if self._device_class == "BaslerGigE":
+            assert (
+                exposure_time <= self._device.ExposureTimeAbs.GetMax()
+                and exposure_time >= self._device.ExposureTimeAbs.GetMin()
+            )
+            self._device.ExposureTimeAbs.SetValue(exposure_time)
+        elif self._device_class == "BaslerUsb":
+            assert (
+                exposure_time <= self._device.ExposureTime.GetMax()
+                and exposure_time >= self._device.ExposureTime.GetMin()
+            )
+            self._device.ExposureTime.SetValue(exposure_time)
+        else:
+            raise ValueError(f"Device class {self._device_class} is not supported!")
+        self._device.Close()
 
     def sample(self):
         """Read latest image from device.
@@ -95,29 +85,13 @@ class BaslerCamera:
         Returns:
             numpy.array: image.
         """
-        time_abs           = datetime.datetime.now(datetime.timezone.utc).astimezone()
-        timeLastPhotoFloat = (time_abs - self.timeLastPhoto).total_seconds()
-
-        if timeLastPhotoFloat >= self.frame_rate_multilog/1000:
-
-            if self.debugMode == False:
-                if self.parameterChange == False:
-                    logger.debug(f"Camera {self.name} taking Photo at {self.timeLastPhoto}")
-                    self.timeLastPhoto = self.round_time(datetime.datetime.now(datetime.timezone.utc).astimezone())
-
-                    grab = self._device.RetrieveResult(self._timeout, pylon.TimeoutHandling_Return)
-                    if grab.GrabSucceeded(): image = self._converter.Convert(grab).GetArray()
-                    else: raise RuntimeError("Image grabbing failed.")
-                    grab.Release()
-                    return image
-
-            else: #if debugmode is true
-                logger.debug(f"Camera {self.name} taking Photo at {self.timeLastPhoto}")
-                self.timeLastPhoto = self.round_time(datetime.datetime.now(datetime.timezone.utc).astimezone())
-                m = np.ones((4000, 3000))
-                n3 = np.arange(3000)
-                return m*n3
-        else: logger.debug(f"Camera {self.name} will take photo in {round(self.frame_rate_multilog/1000-timeLastPhotoFloat,6)} s")
+        grab = self._device.RetrieveResult(self._timeout, pylon.TimeoutHandling_Return)
+        if grab.GrabSucceeded():
+            image = self._converter.Convert(grab).GetArray()
+        else:
+            raise RuntimeError("Image grabbing failed.")
+        grab.Release()
+        return image
 
     def init_output(self, directory="./"):
         """Initialize the output subdirectory and csv file..
@@ -171,111 +145,47 @@ class BaslerCamera:
                 f"{self.name} save_measurement: time difference between event and saving of {timediff} seconds for samplint timestep {time_abs.isoformat(timespec='milliseconds').replace('T', ' ')} - {time_rel}"
             )
         # saving the data:
-        if self.enableDataStorage == True:
-            self.meas_data = sampling
-            img_name = f"img_{self.image_counter:06}.{self.fileformat}"
-            imwrite(f"{self.directory}/{img_name}", sampling)
-            
-            with open(f"{self.directory}/_images.csv", "a", encoding="utf-8") as f:
-                f.write(
-                    f"{timeRightNow.isoformat(timespec='milliseconds').replace('T', ' ')},{time_rel},{img_name},\n"
-                )
-            with open(f"{self.base_directory}/{self.name}.archive.yaml", "a") as f:  # todo
-                f.write(f"  - name: {img_name}\n")
-                f.write(f"    image: {self.name}/{img_name}\n")
-                f.write(f"    timestamp_rel: {time_rel}\n")
-                f.write(
-                    f"    timestamp_abs: {time_abs.isoformat(timespec='milliseconds').replace('T', ' ')}\n"
-                )
-            self.image_counter += 1
-        else:
-            logging.debug("No image saved. self.enableDataStorage is False")
-
-    def _set_exposure_time(self, exposure_time):
-        """Set exposure time."""
-        if self.debugMode == False:
-            logger.info(f"{self.name} - setting exposure time {exposure_time}")
-            self.parameterChange = True
-            time.sleep(0.1)
-            #self._device.Open()
-            if self._device_class == "BaslerGigE":
-                if exposure_time <= self._device.ExposureTimeAbs.GetMax() and exposure_time >= self._device.ExposureTimeAbs.GetMin():
-                    self._device.ExposureTimeAbs.SetValue(exposure_time)
-                    self.exposure_time = exposure_time
-            elif self._device_class == "BaslerUsb":
-                if exposure_time <= self._device.ExposureTime.GetMax() and exposure_time >= self._device.ExposureTime.GetMin():
-                    self._device.ExposureTime.SetValue(exposure_time)
-                    self.exposure_time = exposure_time
-            else:
-                logger.error(f"Device class {self._device_class} is not supported!")
-            #self._device.Close()
-            self.parameterChange = False
-        else:
-            self.exposure_time = exposure_time
-            logger.info(f"{self.name} - setting exposure time {exposure_time} - DEBUG MODE! No data was send to Camera")
-
-    def get_exposure_time(self):
-        return self.exposure_time
+        self.meas_data = sampling
+        img_name = f"img_{self.image_counter:06}.{self.fileformat}"
+        imwrite(f"{self.directory}/{img_name}", sampling)
+        
+        with open(f"{self.directory}/_images.csv", "a", encoding="utf-8") as f:
+            f.write(
+                f"{timeRightNow.isoformat(timespec='milliseconds').replace('T', ' ')},{time_rel},{img_name},\n"
+            )
+        with open(f"{self.base_directory}/{self.name}.archive.yaml", "a") as f:  # todo
+            f.write(f"  - name: {img_name}\n")
+            f.write(f"    image: {self.name}/{img_name}\n")
+            f.write(f"    timestamp_rel: {time_rel}\n")
+            f.write(
+                f"    timestamp_abs: {time_abs.isoformat(timespec='milliseconds').replace('T', ' ')}\n"
+            )
+        self.image_counter += 1
 
     def set_frame_rate(self, frame_rate):
         """Set frame rate for continous sampling. The latest frame is
         then grabbed by the sample function."""
-        if self.debugMode == False:
-            logger.info(f"{self.name} - setting frame rate {frame_rate}")
-            self.parameterChange = True
-            time.sleep(0.1)
-            self._device.AcquisitionFrameRateEnable.SetValue(True)
-            if self._device_class == "BaslerGigE":
-                assert (
-                    frame_rate <= self._device.AcquisitionFrameRateAbs.GetMax()
-                    and frame_rate >= self._device.AcquisitionFrameRateAbs.GetMin()
-                )
-                self._device.AcquisitionFrameRateAbs.SetValue(frame_rate)
-                self.frame_rate = frame_rate
-            elif self._device_class == "BaslerUsb":
-                assert (
-                    frame_rate <= self._device.AcquisitionFrameRate.GetMax()
-                    and frame_rate >= self._device.AcquisitionFrameRate.GetMin()
-                )
-                self._device.AcquisitionFrameRate.SetValue(frame_rate)
-                self.frame_rate = frame_rate
-            else:
-                raise ValueError(f"Device class {self._device_class} is not supported!")
-            self.parameterChange = False
+        self._device.Open()
+        self._device.AcquisitionFrameRateEnable.SetValue(True)
+        if self._device_class == "BaslerGigE":
+            assert (
+                frame_rate <= self._device.AcquisitionFrameRateAbs.GetMax()
+                and frame_rate >= self._device.AcquisitionFrameRateAbs.GetMin()
+            )
+            self._device.AcquisitionFrameRateAbs.SetValue(frame_rate)
+        elif self._device_class == "BaslerUsb":
+            assert (
+                frame_rate <= self._device.AcquisitionFrameRate.GetMax()
+                and frame_rate >= self._device.AcquisitionFrameRate.GetMin()
+            )
+            self._device.AcquisitionFrameRate.SetValue(frame_rate)
         else:
-            self.frame_rate = frame_rate
-            logger.info(f"{self.name} - setting exposure time {frame_rate} - DEBUG MODE! No data was send to Camera")
-
-    def get_frame_rate(self):
-        return self.frame_rate
-
-    def set_frame_rate_multilog(self, framerate):
-        self.frame_rate_multilog = framerate
-
-    def get_frame_rate_multilog(self):
-        return self.frame_rate_multilog
-
-    def setEnableDataStorage(self, enable):
-        self.enableDataStorage = enable
-    
-    def getEnableDataStorage(self):
-        return self.enableDataStorage
-
-    def round_time(self, dt):
-        s = dt.strftime('%Y-%m-%d %H:%M:%S.%f%z')
-        head = s.split(".")[0]
-        tail = "." + s.split(".")[1].split("+")[0]
-        tz = s[-5:]
-        f = float(tail)
-        temp = "{:.01f}".format(f) 
-        new_tail = temp[1:] # temp[0] is always '0'; get rid of it
-        print(head + new_tail)
-        return datetime.datetime.strptime(head + new_tail + tz, '%Y-%m-%d %H:%M:%S.%f%z')
+            raise ValueError(f"Device class {self._device_class} is not supported!")
+        self._device.Close()
 
     def __del__(self):
         """Stopp sampling, reset device."""
         logger.debug(f"Deleting balser camera {self}")
-        if self.debugMode == False:
-            self._device.StopGrabbing()
-            self._device.Close()
+        self._device.StopGrabbing()
+        self._device.Close()
         logger.debug(f"Stopped grabbing and closed device.")
--- multilog/view/basler_camera.py
+++ multilog/view/basler_camera.py
@@ -1,20 +1,5 @@
 import logging
-import time
 import numpy as np
-from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QFont
-from PyQt5.QtWidgets import (
-    QWidget,
-    QVBoxLayout,
-    QGridLayout,
-    QFrame,
-    QLineEdit,
-    QSplitter,
-    QLabel,
-    QCheckBox,
-    QGroupBox,
-    QLineEdit,
-)
 import pyqtgraph as pg
 
 from .base_classes import ImageWidget
@@ -22,20 +7,6 @@ from ..devices.basler_camera import BaslerCamera
 
 logger = logging.getLogger(__name__)
 
-class LineEdit(QLineEdit):
-    """Modified of QLineEdit: red color if modified and not saved."""
-
-    def __init__(self, parent=None):
-        super(LineEdit, self).__init__(parent)
-
-    def focusInEvent(self, e):
-        super(LineEdit, self).focusInEvent(e)
-        self.setStyleSheet("color: red")
-        self.selectAll()
-
-    def mousePressEvent(self, e):
-        self.setStyleSheet("color: red")
-        self.selectAll()
 
 class BaslerCameraWidget(ImageWidget):
     def __init__(self, basler_camera: BaslerCamera, parent=None):
@@ -47,114 +18,6 @@ class BaslerCameraWidget(ImageWidget):
         """
         logger.info(f"Setting up BaslerCameraWidget  for device {basler_camera.name}")
         super().__init__(parent)
-        self.cam = basler_camera
-
-        # Camera Settings
-        self.group_box_plot = QGroupBox("Camera settings")
-        self.group_box_plot_layout = QGridLayout()
-        self.group_box_plot.setLayout(self.group_box_plot_layout)
-        self.parameter_layout.addWidget(self.group_box_plot)
-        self.parameter_layout.setAlignment(self.group_box_plot, Qt.AlignTop)
-
-        # Exposure Time
-        self.lbl_exp_edit = QLabel("Exposure Time [ms]: ")
-        self.lbl_exp_edit.setFont(QFont("Times", 12))
-        self.lbl_exp_edit.setAlignment(Qt.AlignRight)
-        self.group_box_plot_layout.addWidget(self.lbl_exp_edit, 0, 0, 1, 1)
-        self.group_box_plot_layout.setAlignment(self.lbl_exp_edit, Qt.AlignBottom)
-
-        self.edit_exp_edit = LineEdit()
-        self.edit_exp_edit.setFixedWidth(90)
-        self.edit_exp_edit.setFont(QFont("Times", 14, QFont.Bold))
-        self.edit_exp_edit.setText(str(basler_camera.get_exposure_time()))
-        self.group_box_plot_layout.addWidget(self.edit_exp_edit, 0, 1, 1, 1)
-        self.group_box_plot_layout.setAlignment(self.edit_exp_edit, Qt.AlignBottom)
-        self.edit_exp_edit.editingFinished.connect(self.update_edit_exp)
-
-        # Camera Frame Rate
-        self.lbl_fps_edit = QLabel("Device Framerate [ms]: ")
-        self.lbl_fps_edit.setFont(QFont("Times", 12))
-        self.lbl_fps_edit.setAlignment(Qt.AlignRight)
-        self.group_box_plot_layout.addWidget(self.lbl_fps_edit, 1, 0, 1, 1)
-        self.group_box_plot_layout.setAlignment(self.lbl_fps_edit, Qt.AlignBottom)
-
-        self.edit_fps_edit = LineEdit()
-        self.edit_fps_edit.setFixedWidth(90)
-        self.edit_fps_edit.setFont(QFont("Times", 14, QFont.Bold))
-        self.edit_fps_edit.setText(str(basler_camera.get_frame_rate()))
-        self.group_box_plot_layout.addWidget(self.edit_fps_edit, 1, 1, 1, 1)
-        self.group_box_plot_layout.setAlignment(self.edit_fps_edit, Qt.AlignBottom)
-        self.edit_fps_edit.editingFinished.connect(self.update_fps)
-        
-        # Multilog Frame Rate
-        self.lbl_fps_multi = QLabel("Saving Framerate [ms]: ")
-        self.lbl_fps_multi.setFont(QFont("Times", 12))
-        self.lbl_fps_multi.setAlignment(Qt.AlignRight)
-        self.group_box_plot_layout.addWidget(self.lbl_fps_multi, 2, 0, 1, 1)
-        self.group_box_plot_layout.setAlignment(self.lbl_fps_multi, Qt.AlignBottom)
-
-        self.edit_fps_multi = LineEdit()
-        self.edit_fps_multi.setFixedWidth(90)
-        self.edit_fps_multi.setFont(QFont("Times", 14, QFont.Bold))
-        self.edit_fps_multi.setText(str(basler_camera.get_frame_rate_multilog())) # EDIT 
-        self.group_box_plot_layout.addWidget(self.edit_fps_multi, 2, 1, 1, 1)
-        self.group_box_plot_layout.setAlignment(self.edit_fps_multi, Qt.AlignBottom)
-        self.edit_fps_multi.editingFinished.connect(self.update_fps_multi)
-        
-        # Data Storage Checkbox
-        self.lbl_emty = QLabel(" ")
-        self.lbl_emty.setFont(QFont("Times", 12))
-        self.group_box_plot_layout.addWidget(self.lbl_emty, 3, 0, 1, 1)
-
-        self.lbl_data = QLabel("Write screenshots to files: ")
-        self.lbl_data.setFont(QFont("Times", 12))
-        self.lbl_data.setStyleSheet("color: green")
-        self.lbl_data.setAlignment(Qt.AlignRight)
-        self.group_box_plot_layout.addWidget(self.lbl_data, 4, 0, 1, 1)
-        self.group_box_plot_layout.setAlignment(self.lbl_data, Qt.AlignBottom)
-
-        self.cb_data = QCheckBox()
-        self.cb_data.setChecked(True)
-        self.cb_data.setFont(QFont("Times", 12))
-        self.cb_data.setEnabled(True)
-        self.group_box_plot_layout.addWidget(self.cb_data, 4, 1, 1, 1)
-        self.group_box_plot_layout.setAlignment(self.cb_data, Qt.AlignBottom)
-        self.cb_data.clicked.connect(self.update_cb_data)
-
-    def update_edit_exp(self):
-        try: newExp = float(self.edit_exp_edit.text().replace(",", "."))
-        except: newExp = self.cam.get_exposure_time()
-        self.cam._set_exposure_time(newExp)
-        time.sleep(0.1)
-        self.edit_exp_edit.setText(str(self.cam.get_exposure_time()))
-        self.edit_exp_edit.setStyleSheet("color: black")
-        self.edit_exp_edit.clearFocus()
-
-    def update_fps(self):
-        try:    newFps = float(self.edit_fps_edit.text().replace(",", "."))
-        except: newFps = self.cam.get_frame_rate()
-        self.cam.set_frame_rate(newFps)
-        time.sleep(0.1)
-        self.edit_fps_edit.setText(str(self.cam.get_frame_rate()))
-        self.edit_fps_edit.setStyleSheet("color: black")
-        self.edit_fps_edit.clearFocus()
-    
-    def update_fps_multi(self):
-        try:    newFps = float(self.edit_fps_multi.text().replace(",", "."))
-        except: newFps = self.cam.get_frame_rate()
-        self.cam.set_frame_rate_multilog(newFps)
-        time.sleep(0.1)
-        self.edit_fps_multi.setText(str(self.cam.get_frame_rate_multilog()))
-        self.edit_fps_multi.setStyleSheet("color: black")
-        self.edit_fps_multi.clearFocus()
-    
-    def update_cb_data(self):
-        if self.cb_data.isChecked():
-            self.cam.setEnableDataStorage(True)
-            self.lbl_data.setStyleSheet("color: green")
-        else:
-            self.cam.setEnableDataStorage(False)
-            self.lbl_data.setStyleSheet("color: red")
 
     def set_initialization_data(self, sampling):
         """Update image with sampling data (used before recording is
